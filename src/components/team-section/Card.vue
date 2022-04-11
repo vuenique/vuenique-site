2be3af18e6e8f4f27d46476e695995c223cb199a
Alex Corlin <alex.corlin6@gmail.com>
2022-04-11 10:33:55 -0700

    Profile pictures updated
    Co-authored-by: Alexander  Haile <ahaile923@users.noreply.github.com>
    Co-authored-by: Miaowen Zeng <MiaowZ@users.noreply.github.com>

--- src/components/team-section/Card.vue
+++ src/components/team-section/Card.vue
@@ -1,20 +1,44 @@
 <script setup lang="ts">
- const props = defineProps(["id", "photo", "name", "github", "linkedin"])
+import AlexCprofile from '../../assets/AlexCprofile.jpg';
+import AlexHprofile from '../../assets/AlexHprofile.jpg';
+import JamesMprofile from '../../assets/JamesMprofile.png';
+import TrevorGprofile from '../../assets/TrevorGprofile.jpg';
+import MiaZprofile from '../../assets/MiaZprofile.png';
+const props = defineProps(['id', 'photo', 'name', 'github', 'linkedin']);
+console.log(props.photo);
 
+function getPhoto(photoName: string): string | undefined {
+  switch (photoName) {
+    case 'AlexCprofile':
+      return AlexCprofile;
+    case 'AlexHprofile':
+      return AlexHprofile;
+    case 'JamesMprofile':
+      return JamesMprofile;
+    case 'TrevorGprofile':
+      return TrevorGprofile;
+    case 'MiaZprofile':
+      return MiaZprofile;
+    default:
+      console.error(`Invalid photo ${photoName}`);
+      return undefined;
+  }
+}
 </script>
 
 <template>
-<!-- outer card container -->
+  <!-- outer card container -->
   <div class="flex flex-col items-center space-y-2">
     <!-- circular image holder -->
     <div class="w-24 h-24 mt-10">
-      <div class="p-1 rounded-full bg-gradient-to-r from-emerald-400 to-green-700 
-          shadow-lg
-          duration-500
-          hover:scale-y-110 hover:scale-x-110
-          hover:shadow-emerald-400
-          ">
-        <img src="../../assets/catUser.png" alt="Profile photo" class="rounded-full"/>
+      <div
+        class="p-1 rounded-full bg-gradient-to-r from-emerald-400 to-green-700 shadow-lg duration-500 hover:scale-y-110 hover:scale-x-110 hover:shadow-emerald-400"
+      >
+        <img
+          :src="getPhoto(props.photo)"
+          alt="Profile photo"
+          class="rounded-full"
+        />
       </div>
     </div>
     <!-- bios maybe? -->
@@ -23,16 +47,30 @@
     </div>
     <!-- links -->
     <div class="flex space-x-3">
-      <a :href="props.github" target="_blank" rel="noopener noreferrer" title="Github">
-        <img src="../../assets/githubLogo.png" alt="Github" class="w-5 h-5"/>
+      <a
+        :href="props.github"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Github"
+      >
+        <img src="../../assets/githubLogo.png" alt="Github" class="w-5 h-5" />
       </a>
-      <a :href="props.linkedin" target="_blank" rel="noopener noreferrer" title="LinkedIn">
-        <img src="../../assets/linkedinLogo.svg" alt="LinkedIn"  class="w-5 h-5"/>
+      <a
+        :href="props.linkedin"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="LinkedIn"
+      >
+        <img
+          src="../../assets/linkedinLogo.svg"
+          alt="LinkedIn"
+          class="w-5 h-5"
+        />
       </a>
     </div>
   </div>
 </template>
 
-<style>
+<style></style>
 
-</style>
+<!--AlexH: https://ca.slack-edge.com/T02QNTJKVFB-U0301CQUQ1K-2260eb31149e-192-->
--- src/server/controller.js
+++ src/server/controller.js
@@ -1,22 +1,31 @@
 /* eslint-disable no-undef */
 const path = require('path');
-const demoModal = require("./models/model.js");
-const mongoose = require("mongoose");
+const modal = require('./models/model.js');
+const mongoose = require('mongoose');
 const Controller = {};
 
-
-Controller.dataController = async (req, res, next) => {
-   try {
-      const data = await demoModal.find({})
-      console.log(data[0].LetterFrequency)
-      res.locals.data = data[0].LetterFrequency; 
-      return next();
-   }
-   catch(err) {
-    return next({log: "error in dataController" + err});
-   }
+Controller.barData = async (req, res, next) => {
+  try {
+    const data = await modal.barModal.find({});
+    console.log(data[0].LetterFrequency);
+    res.locals.data = data[0].LetterFrequency;
+    return next();
+  } catch (err) {
+    return next({ log: 'error in barData Controller' + err });
+  }
   //res.locals.data = 5;
-  
-}
+};
+
+Controller.linePathData = async (req, res, next) => {
+  try {
+    const data = await modal.lineModal.find({});
+    console.log(data[0].LPData);
+    res.locals.data = data[0].LPData;
+    return next();
+  } catch (err) {
+    return next({ log: 'error in linePathData controller' + err });
+  }
+  //res.locals.data = 5;
+};
 
 module.exports = Controller;
