a51109bc1d891206aa85b7a48b9dd96bf3610687
Alex Corlin <alex.corlin6@gmail.com>
2022-04-12 09:29:51 -0700

    GitHub connected

--- src/components/features-section/FeatureItems/FeatureItem3.vue
+++ src/components/features-section/FeatureItems/FeatureItem3.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="py-4 text-whiteB font-light">
-    <h1
-      class="font-medium text-2xl bg-gradient-to-r from-greenP to-whiteB text-transparent bg-clip-text"
-    >
-      Feature 3
-    </h1>
+  <div class="text-whiteB font-light">
+    <h1 class="py-4 font-medium text-2xl text-whiteB">Feature 3</h1>
     Testing this feature item component out with a longer string of text to see
     what the layout looks like with a more filled in feature description.Testing
     this feature item component out with a longer string of text to see what the
--- src/components/installation-section/startingItems/Documentation.vue
+++ src/components/installation-section/startingItems/Documentation.vue
@@ -2,9 +2,17 @@
 
 <template>
   <h1
-    class="py-4 text-2xl text-whiteB hover:text-white hover:underline underline-offset-1"
+    class="py-4 text-2xl text-whiteB hover:text-white hover:underline underline-offset-1 max-w-min"
   >
-    Documentation
+    <a
+      href="https://github.com/oslabs-beta/Vuenique"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Documentation"
+      class="max-w-min"
+    >
+      Documentation
+    </a>
   </h1>
   <p class="text-whiteB font-light">
     placeholder text to see how this will look when it's more filled out
