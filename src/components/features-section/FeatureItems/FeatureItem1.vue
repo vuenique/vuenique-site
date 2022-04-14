e6fd24e29ffb725cc1b9b6545f727c9f0d4f8b75
Alex Corlin <alex.corlin6@gmail.com>
2022-04-13 17:14:37 -0700

    Features Updated

--- src/components/features-section/FeatureItems/FeatureItem1.vue
+++ src/components/features-section/FeatureItems/FeatureItem1.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div class="text-whiteB font-light">
-    <h1 class="font-medium text-2xl text-whiteB py-4">Feature 1</h1>
-    Testing this feature item component out with a longer string of text to see
-    what the layout looks like with a more filled in feature description.Testing
-    this feature item component out with a longer string of text to see what the
-    layout looks like with a more filled in feature description.
+    <h1 class="font-medium text-2xl text-whiteB py-4">DOM Freedom</h1>
+    Vuenique has built in functions and components which work around the D3
+    library, giving users the power of D3 functionalities while ridding the
+    process of interacting with D3 directly. This avoids DOM repetition using
+    Vue and D3 at the same time and enables users to write more modularized code
   </div>
 </template>
 
